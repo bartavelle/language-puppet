706e2344188e8a827cacdee8a97c2d010d8824eb
bartavelle <bartavelle@gmail.com>
2012-05-11 17:44:49 +0200

    Displays virtuality

--- Puppet/Printers.hs
+++ Puppet/Printers.hs
@@ -2,5 +2,5 @@ module Puppet.Printers where
 
 import Puppet.Interpreter.Types
 
-showRes (CResource crid rname rtype params relations virtuality pos) = putStrLn $ rtype ++ " " ++ show rname ++ " " ++ show params ++ " " ++ show relations
+showRes (CResource crid rname rtype params relations virtuality pos) = putStrLn $ rtype ++ " " ++ show rname ++ " " ++ show params ++ " " ++ show relations ++ " " ++ show virtuality
 showRRes (RResource crid rname rtype params relations pos) = putStrLn $ rtype ++ " " ++ show rname ++ " " ++ show params ++ " " ++ show relations
