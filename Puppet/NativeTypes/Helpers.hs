4f398d0e37c14d44218954e3a2cf3c562c49cb34
bartavelle <bartavelle@gmail.com>
2012-05-19 13:12:52 +0200

    Finished defaultValidate

--- Puppet/NativeTypes/Helpers.hs
+++ Puppet/NativeTypes/Helpers.hs
@@ -1,6 +1,7 @@
 module Puppet.NativeTypes.Helpers where
 
 import Puppet.Interpreter.Types
+import qualified Data.Map as Map
 
 type PuppetTypeName = String
 type PuppetTypeValidate = RResource -> Either String RResource
@@ -12,6 +13,11 @@ faketype tname = (tname, \x -> Right x)
 defaulttype :: PuppetTypeName -> (PuppetTypeName, PuppetTypeMethods)
 defaulttype tname = (tname, defaultValidate)
 
+-- this helper validates any resource
+-- it fills the name and title parameter if necessary
 defaultValidate :: PuppetTypeValidate
-defaultValidate res = Right res
---(res { rrparams = ((rrparams res) ++ name ++ title) } )
+defaultValidate res = Right (res { rrparams = newparams } )
+    where
+        newparams = Map.union defaults  (rrparams res) 
+        defaults  = Map.fromList [("name", nm),("title", nm)]
+        nm = ResolvedString $ rrname res
