b4531d8c16de82676744421ab942e1c70ee7bcee
bartavelle <bartavelle@gmail.com>
2013-10-12 15:22:53 +0200

    WIP

--- PuppetDB/Rest.hs
+++ PuppetDB/Rest.hs
@@ -1,6 +1,9 @@
 module PuppetDB.Rest where
 
 import Puppet.Utils
+import Puppet.PP
+
+import Puppet.Interpreter.Types
 
 import Network.HTTP.Conduit
 import qualified Network.HTTP.Types as W
@@ -18,8 +21,8 @@ import Debug.Trace
 
 runRequest req = do
     let doRequest = withManager (fmap responseBody . httpLbs req) :: IO L.ByteString
-        eHandler :: X.SomeException -> IO (Either String  L.ByteString)
-        eHandler e = return $ Left $ show e ++ ", with queryString " ++ BC.unpack (queryString req)
+        eHandler :: X.SomeException -> IO (Either Doc L.ByteString)
+        eHandler e = return $ Left $ string (show e) <> ", with queryString " <+> string (BC.unpack (queryString req))
     liftIO (fmap Right doRequest `X.catch` eHandler) >>= \case
         Right o -> do
             let utf8 = IConv.convert "LATIN1" "UTF-8" o
@@ -28,14 +31,14 @@ runRequest req = do
                 Nothing                  -> throwError "Json decoding has failed"
         Left err -> throwError err
 
-pdbRequest :: (FromJSON a, ToJSON b) => T.Text -> T.Text -> b -> IO (S.Either String a)
+pdbRequest :: (FromJSON a, ToJSON b) => T.Text -> T.Text -> b -> IO (S.Either Doc a)
 pdbRequest url querytype query = fmap strictifyEither $ runErrorT $ do
-    unless (querytype `elem` ["resources", "nodes", "facts"]) (throwError $ "Invalid query type " ++ T.unpack querytype)
+    unless (querytype `elem` ["resources", "nodes", "facts"]) (throwError $ "Invalid query type" <+> ttext querytype)
     let jsonquery = L.toStrict (encode query)
     q <- case querytype of
              "facts" -> case decode (encode [query]) of -- :(
                                          Just [t] -> return (T.cons '/' t)
-                                         x -> traceShow (encode query) $ throwError $ "Invalid query for facts, need a string: " ++ show x
+                                         x -> traceShow (encode query) $ throwError $ "Invalid query for facts, need a string:" <+> string (show x)
              _       -> return $ T.decodeUtf8 $ "?" <> W.renderSimpleQuery False [("query", jsonquery)]
     let fullurl = url <> "/v1/" <> querytype <> q
     initReq <- case (parseUrl (T.unpack fullurl) :: Maybe (Request a)) of
@@ -43,4 +46,13 @@ pdbRequest url querytype query = fmap strictifyEither $ runErrorT $ do
         Nothing -> throwError "Something failed when parsing the PuppetDB URL"
     let req = initReq { requestHeaders = [("Accept", "application/json")] }
     runRequest req
+
+pdbConnect :: T.Text -> IO (Either String PuppetDBAPI)
+pdbConnect url = return $ Right $ PuppetDBAPI
+    (const (return (S.Left "operation not supported")))
+    (const (return (S.Left "operation not supported")))
+    (const (return (S.Left "operation not supported")))
+    (pdbRequest url "facts")
+    (pdbRequest url "resources")
+    (return (S.Left "operation not supported"))
 
