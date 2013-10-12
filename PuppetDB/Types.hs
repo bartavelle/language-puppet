40575276cf66885b2898fe97feb0db04e80f6579
bartavelle <bartavelle@gmail.com>
2013-10-12 15:14:52 +0200

    WIP

---
+++ PuppetDB/Types.hs
@@ -0,0 +1,13 @@
+module PuppetDB.Types where
+
+import qualified Data.Text as T
+import Puppet.Interpreter.Types
+import Data.Aeson
+import qualified Data.ByteString as BS
+import Control.Applicative
+import qualified Data.Vector as V
+
+-- TODO
+type FullCatalog = T.Text
+
+
