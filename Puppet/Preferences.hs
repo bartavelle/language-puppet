c8361a424dfd3fa76f08a4a2e858d707bd82c27b
bartavelle <bartavelle@gmail.com>
2014-01-28 14:15:16 +0100

    Fix preferences

--- Puppet/Preferences.hs
+++ Puppet/Preferences.hs
@@ -17,7 +17,6 @@ data Preferences = Preferences
     { _manifestPath    :: FilePath -- ^ The path to the manifests.
     , _modulesPath     :: FilePath -- ^ The path to the modules.
     , _templatesPath   :: FilePath -- ^ The path to the template.
-    , _compilePoolSize :: Int -- ^ Size of the compiler pool.
     , _prefPDB         :: PuppetDBAPI
     , _natTypes        :: Container PuppetTypeMethods -- ^ The list of native types.
     , _prefExtFuncs    :: Container ( [PValue] -> InterpreterMonad PValue )
@@ -34,4 +33,4 @@ genPreferences basedir = do
         templatedir = basedir <> "/templates"
     typenames <- fmap (map takeBaseName) (getFiles (T.pack modulesdir) "lib/puppet/type" ".rb")
     let loadedTypes = HM.fromList (map defaulttype typenames)
-    return $ Preferences manifestdir modulesdir templatedir 8 dummyPuppetDB (baseNativeTypes `HM.union` loadedTypes) (stdlibFunctions) (Just (basedir <> "/hiera.yaml"))
+    return $ Preferences manifestdir modulesdir templatedir dummyPuppetDB (baseNativeTypes `HM.union` loadedTypes) (stdlibFunctions) (Just (basedir <> "/hiera.yaml"))
