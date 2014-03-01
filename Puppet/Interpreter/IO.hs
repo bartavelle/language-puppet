bc85bc847a217a24db20fa412816e306468e2364
bartavelle <bartavelle@gmail.com>
2014-03-01 13:54:37 +0100

    Simplify the Program transformation
    This patch gets rid of the intermediate monad stack transformer, that
    was actually useless.

--- Puppet/Interpreter/IO.hs
+++ Puppet/Interpreter/IO.hs
@@ -22,85 +22,71 @@ import qualified Data.ByteString as BS
 import qualified Scripting.Lua as Lua
 
 import qualified Data.Either.Strict as S
-import Data.Maybe (isJust)
 import GHC.Stack
 import Debug.Trace (traceEventIO)
 import Text.Regex.PCRE.ByteString
 import Text.Regex.PCRE.ByteString.Utils
 
-canfailIO :: IO (S.Either Doc a) -> ErrorT Doc (RSST InterpreterReader InterpreterWriter InterpreterState IO) a
-canfailIO m = liftIO (eitherDocIO m) >>= \case
-    S.Right x -> return x
-    S.Left y -> throwPosError y
-
 bs :: BS.ByteString -> Doc
 bs = string . show
 
 interpretIO :: InterpreterReader
+            -> InterpreterState
             -> InterpreterMonad a
-            -> RSMIO a
-interpretIO rdr = intexpr . viewT
+            -> IO (Either Doc a, InterpreterState, InterpreterWriter)
+interpretIO rdr prmstate instr = case runState (viewT instr) prmstate of
+                                     (!a,!nextstate) -> evalInstr nextstate a
     where
         pdb = _pdbAPI rdr
-        runC :: RSMIO b -> (b -> InterpreterMonad a) -> RSMIO a
-        runC c f = do
-            o <- c
-            interpretIO rdr (f o)
-        intexpr :: State InterpreterState (ProgramViewT InterpreterInstr (State InterpreterState) a) -> RSMIO a
-        intexpr computation = do
-            initstate <- get
-            case runState computation initstate of
-                (!a,!nextstate) -> put nextstate >> evalInstr a
-        evalInstr :: ProgramViewT InterpreterInstr (State InterpreterState) a -> RSMIO a
-        evalInstr (Return x) = return x
-        evalInstr (WriterTell t :>>= f) = tell t >> runC (return ()) f
-        evalInstr (WriterPass m :>>= f) = runC (pass (interpretIO rdr m)) f
-        evalInstr (WriterListen m :>>= f) = runC (listen (interpretIO rdr m)) f
-        evalInstr (GetNativeTypes :>>= f) = interpretIO rdr (f (rdr ^. nativeTypes))
-        evalInstr (ExternalFunction fname args :>>= f) = case rdr ^. externalFunctions . at fname of
-                                                                Just fn -> interpretIO rdr ( fn args >>= f)
-                                                                Nothing -> throwPosError ("Unknown function: " <> ttext fname)
-        evalInstr (ErrorThrow d :>>= _) = throwError d
-        evalInstr (ErrorCatch m h :>>= f) = runC (catchError (interpretIO rdr m) (interpretIO rdr . h)) f
-        evalInstr (GetNodeName :>>= f) = interpretIO rdr (f (rdr ^. thisNodename))
-
-        evalInstr (GetStatement topleveltype toplevelname :>>= f) = runC (canfailIO ((rdr ^. getStatement) topleveltype toplevelname)) f
-        evalInstr (ComputeTemplate fn scp cscps :>>= f) = runC (canfailIO ((rdr ^. computeTemplateFunction) fn scp cscps)) f
-        evalInstr (HieraQuery scps q t :>>= f) = runC (canfailIO ((rdr ^. hieraQuery) scps q t)) f
-        evalInstr (GetCurrentCallStack :>>= f) = runC (liftIO currentCallStack) f
-        evalInstr (ReadFile fls :>>= f) = runC (canfailIO (file fls)) f
-            where
-                file :: [T.Text] -> IO (S.Either Doc T.Text)
-                file [] = return $ S.Left ("No file found in" <+> list (map ttext fls))
-                file (x:xs) = fmap S.Right (T.readFile (T.unpack x)) `catch` (\SomeException{} -> file xs)
-        evalInstr (TraceEvent e :>>= f) = runC (liftIO (traceEventIO e)) f
-        evalInstr (PDBInformation :>>= f) = runC (liftIO (pdbInformation pdb)) f
-        evalInstr (PDBReplaceCatalog w :>>= f) = runC (canfailIO (replaceCatalog pdb w)) f
-        evalInstr (PDBReplaceFacts fcts :>>= f) = runC (canfailIO (replaceFacts pdb fcts)) f
-        evalInstr (PDBDeactivateNode nn :>>= f) = runC (canfailIO (deactivateNode pdb nn)) f
-        evalInstr (PDBGetFacts q :>>= f) = runC (canfailIO (getFacts pdb q)) f
-        evalInstr (PDBGetResources q :>>= f) = runC (canfailIO (getResources pdb q)) f
-        evalInstr (PDBGetNodes q :>>= f) = runC (canfailIO (getNodes pdb q)) f
-        evalInstr (PDBCommitDB :>>= f) = runC (canfailIO (commitDB pdb)) f
-        evalInstr (PDBGetResourcesOfNode nn q :>>= f) = runC (canfailIO (getResourcesOfNode pdb nn q)) f
-        evalInstr (SubstituteCompile regexp target replacement :>>= f) = liftIO (substituteCompile regexp target replacement) >>= \case
-            Left rr -> throwPosError ("regsubst" <> parens (bs regexp <> comma <> bs replacement) <> ":" <+> string rr)
-            Right x -> interpretIO rdr (f x)
-        evalInstr (SplitCompile splt src :>>= f) = liftIO (splitCompile splt src) >>= \case
-            Left rr -> throwPosError ("split" <> parens (bs splt <> comma <> bs src) <> ":" <+> string rr)
-            Right x -> interpretIO rdr (f x)
-        evalInstr (Compile c e r :>>= f) = liftIO (compile c e r) >>= \case
-            Left rr -> throwPosError ("compile" <> parens (bs r) <> ":" <+> string (show rr))
-            Right x -> interpretIO rdr (f x)
-        evalInstr (Execute rv va :>>= f) = liftIO (execute rv va) >>= \case
-            Left rr -> throwPosError ("execute" <> parens ("/regexp/" <> comma <>  bs va) <> ":" <+> string (show rr))
-            Right x -> interpretIO rdr $ f (isJust x )
-        evalInstr (CallLua c fname args :>>= f) = runC runlua f
-            where
-                runlua = do
-                    r <- liftIO $ withMVar c $ \stt ->
-                        catch (fmap Right (Lua.callfunc stt (T.unpack fname) args)) (\e -> return $ Left $ show (e :: SomeException))
-                    case r of
-                        Right x -> return x
-                        Left rr -> throwPosError (string rr)
+        evalInstr :: InterpreterState -> ProgramViewT InterpreterInstr (State InterpreterState) a -> IO (Either Doc a, InterpreterState, InterpreterWriter)
+        evalInstr stt (Return x) = return (Right x, stt, mempty)
+        evalInstr stt (a :>>= f) =
+            let runC a' = interpretIO rdr stt (f a')
+                err rs = return (Left rs, stt, mempty)
+                canFailIO iof = iof >>= \case
+                    S.Left rr -> err rr
+                    S.Right x -> runC x
+                runlua c fname args = liftIO $ withMVar c $ \lstt ->
+                        catch (fmap Right (Lua.callfunc lstt (T.unpack fname) args)) (\e -> return $ Left $ show (e :: SomeException))
+                strFailIO iof errf = iof >>= \case
+                    Left rr -> err (errf (string rr))
+                    Right x -> runC x
+                file :: [T.Text] -> IO (Either String T.Text)
+                file [] = return $ Left ""
+                file (x:xs) = fmap Right (T.readFile (T.unpack x)) `catch` (\SomeException{} -> file xs)
+            in  case a of
+                    ExternalFunction fname args  -> case rdr ^. externalFunctions . at fname of
+                                                        Just fn -> interpretIO rdr stt ( fn args >>= f)
+                                                        Nothing -> err ("Unknown function: " <> ttext fname)
+                    GetStatement topleveltype toplevelname
+                                                 -> canFailIO ((rdr ^. getStatement) topleveltype toplevelname)
+                    ComputeTemplate fn scp cscps -> canFailIO ((rdr ^. computeTemplateFunction) fn scp cscps)
+                    WriterTell t                 -> (_3 %~ (t <>)) `fmap` runC ()
+                    WriterPass _                 -> error "WriterPass"
+                    WriterListen _               -> error "WriterListen"
+                    GetNativeTypes               -> runC (rdr ^. nativeTypes)
+                    ErrorThrow d                 -> err d
+                    ErrorCatch _ _               -> error "ErrorCatch"
+                    GetNodeName                  -> runC (rdr ^. thisNodename)
+                    HieraQuery scps q t          -> canFailIO ((rdr ^. hieraQuery) scps q t)
+                    GetCurrentCallStack          -> liftIO currentCallStack >>= runC
+                    ReadFile fls                 -> strFailIO (file fls) (const $ "No file found in " <> list (map ttext fls))
+                    TraceEvent e                 -> traceEventIO e >>= runC
+                    PDBInformation               -> pdbInformation pdb >>= runC
+                    PDBReplaceCatalog w          -> canFailIO (replaceCatalog pdb w)
+                    PDBReplaceFacts fcts         -> canFailIO (replaceFacts pdb fcts)
+                    PDBDeactivateNode nn         -> canFailIO (deactivateNode pdb nn)
+                    PDBGetFacts q                -> canFailIO (getFacts pdb q)
+                    PDBGetResources q            -> canFailIO (getResources pdb q)
+                    PDBGetNodes q                -> canFailIO (getNodes pdb q)
+                    PDBCommitDB                  -> canFailIO (commitDB pdb)
+                    PDBGetResourcesOfNode nn q   -> canFailIO (getResourcesOfNode pdb nn q)
+                    SplitCompile splt src        -> strFailIO (splitCompile splt src) (\rr -> "split" <> parens (bs splt <> comma <> bs src) <> ":" <+> rr)
+                    SubstituteCompile regexp target replacement
+                                                 -> strFailIO (substituteCompile regexp target replacement) (\rr -> "regsubst" <> parens (bs regexp <> comma <> bs replacement) <> ":" <+> rr)
+                    Compile c e r                -> strFailIO ((_Left %~ show) `fmap` compile c e r) (\rr -> "compile" <> parens (bs r) <> ":" <+> rr)
+                    Execute rv va                -> strFailIO (((_Left %~ show) . (_Right %~ has _Just)) `fmap` execute rv va) (\rr -> "execute" <> parens ("/regexp/" <> comma <>  bs va) <> ":" <+> rr)
+                    CallLua c fname args         -> runlua c fname args >>= \case
+                                                        Right x -> runC x
+                                                        Left rr -> interpretIO rdr stt (throwPosError (string rr))
 
