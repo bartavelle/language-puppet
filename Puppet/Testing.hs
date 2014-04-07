a40da974b9b569f382e2a0d4bc8de21dcc7813ec
bartavelle <bartavelle@gmail.com>
2014-04-07 11:31:29 +0200

    added puppet as a default user/group

--- Puppet/Testing.hs
+++ Puppet/Testing.hs
@@ -89,8 +89,8 @@ usersGroupsDefined = do
     let getResourceType t = c ^.. traverse . filtered (\r -> r ^. rid . itype == t && r ^. rattributes . at "ensure" /= Just "absent")
         users = getResourceType "user"
         groups = getResourceType "group"
-        knownUsers = HS.fromList $ map (view (rid . iname)) users ++ ["root","","syslog","mysql"]
-        knownGroups = HS.fromList $ map (view (rid . iname)) groups ++ ["root", "adm", "syslog", "mysql", "nagios",""]
+        knownUsers = HS.fromList $ map (view (rid . iname)) users ++ ["root","","syslog","mysql","puppet"]
+        knownGroups = HS.fromList $ map (view (rid . iname)) groups ++ ["root", "adm", "syslog", "mysql", "nagios","puppet",""]
         checkResource lensU lensG = mapM_ (checkResource' lensU lensG)
         checkResource' lensU lensG res = do
             let d = "Resource " <> show (pretty res) <> " should have a valid "
