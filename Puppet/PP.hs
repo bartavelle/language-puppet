63c1fff2a28f69e6ebc6dcd0f33649b2e737df4d
Pierre Radermecker <pierrer@pi3r.be>
2014-10-01 21:41:58 +0200

    Remove dead code

--- Puppet/PP.hs
+++ Puppet/PP.hs
@@ -2,7 +2,6 @@ module Puppet.PP
     ( module Text.PrettyPrint.ANSI.Leijen
     , ttext
     , tshow
-    , dq
     , pshow
     , displayNocolor
     ) where
@@ -15,9 +14,6 @@ ttext = text . T.unpack
 
 tshow :: Show a => a -> T.Text
 tshow = T.pack . show
-
-dq :: T.Text -> T.Text
-dq x = T.cons '"' (T.snoc x '"')
 
 pshow :: Doc -> String
 pshow d = displayS (renderPretty 0.4 120 d) ""
