115af7245ef96cd960d046519d8f97f7101f6402
Pierre Radermecker <pierrer@pi3r.be>
2014-12-11 19:37:42 +0100

    Doc: add parse test for selector

--- tests/expr.hs
+++ tests/expr.hs
@@ -1,21 +1,28 @@
 module Main where
 
-import Puppet.Parser
-import Puppet.Parser.PrettyPrinter()
-import Puppet.Parser.Types
-import Text.Parser.Combinators
-import Control.Monad
-import Data.Maybe
-import qualified Data.Text as T
-import Control.Applicative
-import qualified Data.Vector as V
-import Control.Arrow (first)
+import           Control.Applicative
+import           Control.Arrow               (first)
+import           Control.Monad
+import           Data.Maybe
+import qualified Data.Text                   as T
+import           Data.Tuple.Strict
+import qualified Data.Vector                 as V
+import           Puppet.Parser
+import           Puppet.Parser.PrettyPrinter ()
+import           Puppet.Parser.Types
+import           Text.Parser.Combinators
 
 testcases :: [(T.Text, Expression)]
 testcases =
     [ ("5 + 3 * 2", 5 + 3 * 2)
     , ("5+2 == 7", Equal (5 + 2) 7)
     , ("include(foo::bar)",  Terminal (UFunctionCall "include" (V.singleton "foo::bar") ))
+    , ("$y ? {\
+     \ undef   => 'undef',\
+     \ default => 'default',\
+    \ }",  ConditionalValue (Terminal (UVariableReference "y"))
+           (V.fromList [SelectorValue (UString "undef") :!: Terminal (UString "undef")
+                       ,SelectorDefault :!: Terminal (UString "default")]))
     , ("$x", Terminal (UVariableReference "x"))
     , ("\"${x}\"", Terminal (UInterpolable (V.fromList [Terminal (UVariableReference "x")])))
     , ("\"${x[3]}\"", Terminal (UInterpolable (V.fromList [Lookup (Terminal (UVariableReference "x")) 3])))
