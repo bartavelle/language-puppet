00cf087429f66cef307f85576443e35881f1295a
Pierre Radermecker <pierrer@pi3r.be>
2015-05-13 23:25:27 +0200

    Update servant to 0.4

--- PuppetDB/Remote.hs
+++ PuppetDB/Remote.hs
@@ -13,45 +13,40 @@ import Control.Monad.Trans.Either
 import qualified Data.Either.Strict as S
 import Servant.API
 import Servant.Client
-import Servant.Common.Text
 import Data.Aeson
 import Data.Proxy
 
-type PDBAPIv3 =    "nodes"     :> QueryParam "query" (Query NodeField)     :> Get [PNodeInfo]
-              :<|> "nodes"     :> Capture "resourcename" Text :> "resources" :> QueryParam "query" (Query ResourceField) :> Get [Resource]
-              :<|> "facts"     :> QueryParam "query" (Query FactField)     :> Get [PFactInfo]
-              :<|> "resources" :> QueryParam "query" (Query ResourceField) :> Get [Resource]
+type PDBAPIv3 =    "nodes"     :> QueryParam "query" (Query NodeField)       :> Get '[JSON] [PNodeInfo]
+              :<|> "nodes"     :> Capture "resourcename" Text :> "resources" :> QueryParam "query" (Query ResourceField) :> Get '[JSON] [Resource]
+              :<|> "facts"     :> QueryParam "query" (Query FactField)       :> Get '[JSON] [PFactInfo]
+              :<|> "resources" :> QueryParam "query" (Query ResourceField)   :> Get '[JSON] [Resource]
 
 type PDBAPI = "v3" :> PDBAPIv3
 
-spdbAPI :: Proxy PDBAPI
-spdbAPI = Proxy
-
-sgetNodes :: Maybe (Query NodeField) -> BaseUrl -> EitherT String IO [PNodeInfo]
-sgetNodeResources :: Text -> Maybe (Query ResourceField) -> BaseUrl -> EitherT String IO [Resource]
-sgetFacts :: Maybe (Query FactField) -> BaseUrl -> EitherT String IO [PFactInfo]
-sgetResources :: Maybe (Query ResourceField) -> BaseUrl -> EitherT String IO [Resource]
-(     sgetNodes
- :<|> sgetNodeResources
- :<|> sgetFacts
- :<|> sgetResources
- ) = client spdbAPI
+api :: Proxy PDBAPI
+api = Proxy
 
 -- | Given an URL (ie. @http://localhost:8080@), will return an incomplete 'PuppetDBAPI'.
 pdbConnect :: BaseUrl -> IO (S.Either PrettyError (PuppetDBAPI IO))
-pdbConnect url = return $ S.Right $ PuppetDBAPI
-    (return (string $ show url))
-    (const (left "operation not supported"))
-    (const (left "operation not supported"))
-    (const (left "operation not supported"))
-    (q1 sgetFacts)
-    (q1 sgetResources)
-    (q1 sgetNodes)
-    (left "operation not supported")
-    (\ndename q -> prettyError $ sgetNodeResources ndename (Just q) url)
-    where
-        prettyError :: EitherT String IO b -> EitherT PrettyError IO b
-        prettyError = bimapEitherT (PrettyError . string) id
-        q1 :: (ToText a, FromJSON b) => (Maybe a -> BaseUrl -> EitherT String IO b) -> a -> EitherT PrettyError IO b
-        q1 f x = prettyError $ f (Just x) url
+pdbConnect url =
+    return $ S.Right $ PuppetDBAPI
+        (return (string $ show url))
+        (const (left "operation not supported"))
+        (const (left "operation not supported"))
+        (const (left "operation not supported"))
+        (q1 sgetFacts)
+        (q1 sgetResources)
+        (q1 sgetNodes)
+        (left "operation not supported")
+        (\ndename q -> prettyError $ sgetNodeResources ndename (Just q))
+        where
+            sgetNodes :: Maybe (Query NodeField) -> EitherT ServantError IO [PNodeInfo]
+            sgetNodeResources :: Text -> Maybe (Query ResourceField) -> EitherT ServantError IO [Resource]
+            sgetFacts :: Maybe (Query FactField) -> EitherT ServantError IO [PFactInfo]
+            sgetResources :: Maybe (Query ResourceField) -> EitherT ServantError IO [Resource]
+            (sgetNodes :<|> sgetNodeResources :<|> sgetFacts :<|> sgetResources) = client api url
 
+            prettyError :: EitherT ServantError IO b -> EitherT PrettyError IO b
+            prettyError = bimapEitherT (PrettyError . string. show) id
+            q1 :: (ToText a, FromJSON b) => (Maybe a -> EitherT ServantError IO b) -> a -> EitherT PrettyError IO b
+            q1 f = prettyError . f . Just
