121540aaf26440853f4b9381b67761d6886c54d9
Pierre Radermecker <pierrer@pi3r.be>
2015-11-21 01:33:13 +0100

    Add missing file

---
+++ Puppet/Paths.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Puppet.Paths where
+
+import Control.Lens
+import Data.Monoid
+
+data PuppetDirPaths = PuppetDirPaths
+    { _baseDir       :: FilePath -- ^ Puppet base working directory
+    , _manifestPath  :: FilePath -- ^ The path to the manifests.
+    , _modulesPath   :: FilePath -- ^ The path to the modules.
+    , _templatesPath :: FilePath -- ^ The path to the template.
+    , _testPath      :: FilePath -- ^ The path to a tests folders to hold tests files such as the pdbfiles.
+    }
+
+makeClassy ''PuppetDirPaths
+
+puppetPaths :: FilePath -> PuppetDirPaths
+puppetPaths basedir = PuppetDirPaths basedir manifestdir modulesdir templatedir testdir
+    where
+        manifestdir = basedir <> "/manifests"
+        modulesdir  = basedir <> "/modules"
+        templatedir = basedir <> "/templates"
+        testdir     = basedir <> "/tests"
