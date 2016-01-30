f3665794cea3745db0f7a85ed4537433390eac62
bartavelle <bartavelle@gmail.com>
2016-01-30 11:22:14 +0100

    Added unit tests for the size function

---
+++ tests/Function/SizeSpec.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE OverloadedLists #-}
+module Function.SizeSpec (spec, main) where
+
+import           Test.Hspec
+
+import           Control.Monad
+import qualified Data.HashMap.Strict as HM
+import           Data.Monoid
+import           Data.Scientific
+
+import           Puppet.Interpreter.Pure
+import           Puppet.Interpreter.Types
+import           Puppet.PP
+import           Puppet.Stdlib
+
+main :: IO ()
+main = hspec spec
+
+evalArgs :: InterpreterMonad PValue -> Either PrettyError Scientific
+evalArgs = dummyEval
+        >=> \pv -> case pv of
+                      PNumber s -> return s
+                      _ -> Left ("Expected a string, not " <> PrettyError (pretty pv))
+
+spec :: Spec
+spec = do
+    sizeFunc <- case HM.lookup "size" stdlibFunctions of
+                    Just f -> return f
+                    Nothing -> fail "Don't know the size function"
+    let evalArgs' = evalArgs . sizeFunc
+    let check args res = case evalArgs' args of
+                             Left rr -> expectationFailure (show rr)
+                             Right res' -> res' `shouldBe` res
+        checkError args ins = case evalArgs' args of
+                                  Left rr -> show rr `shouldContain` ins
+                                  Right r -> expectationFailure ("Should have errored, received this instead: " <> show r)
+    it "should error with no arguments" (checkError [] "a single argument")
+    it "should error with numerical arguments" (checkError [PNumber 1] "size(): Expects ")
+    it "should error with boolean arguments" (checkError [PBoolean True] "size(): Expects ")
+    -- Not conformant:
+    -- it "should error with numerical arguments" (checkError ["1"] "size(): Expects ")
+    it "should handle arrays" $ do
+        check [PArray []] 0
+        check [PArray ["a"]] 1
+        check [PArray ["one","two","three"]] 3
+        check [PArray ["one","two","three","four"]] 4
+    it "should handle hashes" $ do
+        check [PHash []] 0
+        check [PHash [("1","2")]] 1
+        check [PHash [("1","2"),("3","4")]] 2
+    it "should handle strings" $ do
+        check [""] 0
+        check ["a"] 1
+        check ["ab"] 2
+        check ["abcd"] 4
+
--- tests/Spec.hs
+++ tests/Spec.hs
@@ -2,6 +2,7 @@ import Test.Hspec
 
 import qualified InterpreterSpec
 import qualified Function.ShellquoteSpec
+import qualified Function.SizeSpec
 
 main :: IO ()
 main = hspec spec
@@ -10,3 +11,4 @@ spec :: Spec
 spec = do
   describe "Interpreter"  InterpreterSpec.collectorSpec
   describe "The shellquote function"  Function.ShellquoteSpec.spec
+  describe "The size function"  Function.SizeSpec.spec
