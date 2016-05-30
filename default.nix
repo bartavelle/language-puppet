6bdb0a296c02091b4bcf8630a9761e4e85225fa9
Pierre Radermecker <pierrer@pi3r.be>
2016-05-31 00:31:17 +0200

    Update nix files

--- default.nix
+++ default.nix
@@ -1,32 +1,43 @@
 { mkDerivation, aeson, ansi-wl-pprint, attoparsec, base
 , base16-bytestring, bytestring, case-insensitive, containers
-, cryptohash, Diff, directory, either, exceptions, filecache, Glob
-, hashable, hruby, hslogger, hslua, hspec, HUnit, lens, lens-aeson
-, luautils, mtl, operational, optparse-applicative, parallel-io
-, parsec, parsers, pcre-utils, process, regex-pcre-builtin
-, scientific, servant, servant-client, split, stdenv, stm
-, strict-base-types, temporary, text, time, transformers, unix
+, cryptonite, directory, either, exceptions, filecache, formatting
+, Glob, hashable, hruby, hslogger, hslua, hspec, hspec-megaparsec
+, http-api-data, http-client, HUnit, lens, lens-aeson, megaparsec
+, memory, mtl, operational, optparse-applicative, parallel-io
+, parsec, pcre-utils, process, random, regex-pcre-builtin
+, scientific, semigroups, servant, servant-client, split, stdenv
+, stm, strict-base-types, temporary, text, time, transformers, unix
 , unordered-containers, vector, yaml
+, ruby, glpk, pcre, zlib
 }:
 mkDerivation {
   pname = "language-puppet";
-  version = "1.1.3";
+  version = "1.1.5.1";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
-  buildDepends = [
+  libraryHaskellDepends = [
     aeson ansi-wl-pprint attoparsec base base16-bytestring bytestring
-    case-insensitive containers cryptohash Diff directory either
-    exceptions filecache Glob hashable hruby hslogger hslua hspec lens
-    lens-aeson luautils mtl operational optparse-applicative
-    parallel-io parsec parsers pcre-utils process regex-pcre-builtin
-    scientific servant servant-client split stm strict-base-types text
-    time transformers unix unordered-containers vector yaml
+    case-insensitive containers cryptonite directory either exceptions
+    filecache formatting hashable hruby hslogger hslua hspec
+    http-api-data http-client lens lens-aeson megaparsec memory mtl
+    operational parsec pcre-utils process random regex-pcre-builtin
+    scientific semigroups servant servant-client split stm
+    strict-base-types text time transformers unix unordered-containers
+    vector yaml
   ];
-  testDepends = [
-    ansi-wl-pprint base either Glob hspec HUnit lens parsec parsers
-    strict-base-types temporary text unix unordered-containers vector
+  executableHaskellDepends = [
+    aeson base bytestring containers Glob hslogger http-client lens
+    megaparsec mtl optparse-applicative parallel-io regex-pcre-builtin
+    servant-client strict-base-types text transformers
+    unordered-containers vector yaml
   ];
+  testHaskellDepends = [
+    ansi-wl-pprint base Glob hslogger hspec hspec-megaparsec HUnit lens
+    megaparsec mtl scientific strict-base-types temporary text
+    transformers unix unordered-containers vector
+  ];
+  libraryPkgconfigDepends = [ ruby glpk pcre zlib ];
   homepage = "http://lpuppet.banquise.net/";
   description = "Tools to parse and evaluate the Puppet DSL";
   license = stdenv.lib.licenses.bsd3;
