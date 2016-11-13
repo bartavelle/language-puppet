937647c0908d81c9a184fb44e5359e8c23257d76
Pierre Radermecker <pierrer@pi3r.be>
2016-11-13 20:09:52 +0100

    Refactor

--- release.nix
+++ release.nix
@@ -1,9 +1,12 @@
 { supportedSystems ? [ "x86_64-linux"] }:
 
 with (import <nixpkgs/pkgs/top-level/release-lib.nix> { inherit supportedSystems; });
+with (import <nixpkgs/pkgs/development/haskell-modules/lib.nix> { inherit pkgs; });
 
-let hpkgs = pkgs_x86_64_linux.haskellPackages;
+let hpkgs = pkgs.haskellPackages;
+    servant = hpkgs.servant_0_9_1_1;
+    servant-client = hpkgs.servant-client_0_9_1_1;
 in
 {
-  language_puppet = pkgs.lib.hydraJob (hpkgs.callPackage ./language-puppet.nix {servant = hpkgs.servant_0_9_1_1; servant-client = hpkgs.servant-client_0_9_1_1;});
+  language_puppet = pkgs.lib.hydraJob (dontHaddock (hpkgs.callPackage ./language-puppet.nix {inherit servant servant-client;}));
 }
