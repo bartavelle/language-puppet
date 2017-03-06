048cdfe53d75d988d2bdaf158a1a96d6052e378a
bartavelle <bartavelle@gmail.com>
2017-03-06 14:38:26 +0100

    Extend sprintf

--- Puppet/Interpreter/Resolve/Sprintf.hs
+++ Puppet/Interpreter/Resolve/Sprintf.hs
@@ -1,9 +1,16 @@
+{-# LANGUAGE TypeApplications #-}
 module Puppet.Interpreter.Resolve.Sprintf where
 
 import           Control.Applicative
 import           Control.Monad
 import           Data.Attoparsec.Text
-import qualified Data.Text                   as T
+import           Data.Scientific (Scientific)
+import qualified Data.Text                         as T
+import qualified Data.Text.Lazy                    as TL
+import qualified Data.Text.Lazy.Builder            as TB
+import qualified Data.Text.Lazy.Builder.Int        as TB
+import qualified Data.Text.Lazy.Builder.Scientific as TB
+
 
 import           Puppet.Interpreter.Types
 import           Puppet.Interpreter.Utils
@@ -95,24 +102,43 @@ format = do
     return (PrintfFormat flags width prec len ft)
 
 sprintf :: T.Text -> [PValue] -> InterpreterMonad PValue
-sprintf str oargs = PString . mconcat <$> go (parseFormat str) oargs
+sprintf str oargs = PString . TL.toStrict . TB.toLazyText . mconcat <$> go (parseFormat str) oargs
   where
-    go (Raw x : xs) args = (x :) <$> go xs args
-    go (Format (PrintfFormat _ _ _ _ Ts) : xs) (arg : args) =
-        let nstr = case arg of
-                      PString s -> s
-                      _ -> T.pack (show (pretty arg))
-        in  (nstr:) <$> go xs args
+    go (Raw x : xs) args = (TB.fromText x :) <$> go xs args
+    go (Format f : _) _ | Hash `elem` _pfFlags f = throwPosError "sprintf: the # modifier is not supported"
     go (Format f : xs) (arg : args) = do
-        sc <- case arg of
-                  PNumber n -> pure n
-                  PString s -> maybe (throwError "sprintf: Don't know how to convert this to a number") return (text2Scientific s)
-                  _         -> throwError "sprintf: Don't know how to convert this to a number"
-        case _pfType f of
-            Td -> cont $ T.pack (show (truncate sc :: Integer))
-            _ -> throwPosError "sprintf: not yet supported"
-        where
-          cont formatted = (formatted:) <$> go xs args
+        let numeric = case arg of
+                          PNumber n -> pure n
+                          PString s -> maybe (throwError "sprintf: Don't know how to convert this to a number") return (text2Scientific s)
+                          _         -> throwError "sprintf: Don't know how to convert this to a number"
+            flags = _pfFlags f
+            sh mkBuilder n | has Minus            = TL.justifyLeft padlen ' ' (sprefix <> content)
+                           | has Plus && has Zero = sprefix <> TL.justifyRight mpadlen '0' content
+                           | has Plus             = TL.justifyRight padlen ' ' (sprefix <> content)
+                           | has Zero             = TL.justifyRight padlen '0' content
+                           | otherwise            = TL.justifyRight padlen ' ' content
+                 where
+                   (mpadlen, sprefix) | Plus  `elem` flags && n >= 0 = (padlen - 1, "+")
+                                      | Space `elem` flags && n >= 0 = (padlen - 1, " ")
+                                      | otherwise = (padlen, mempty)
+                   padlen = maybe 0 fromIntegral (_pfWidth f)
+                   has flg = flg `elem` flags
+                   content = TB.toLazyText (mkBuilder n)
+        baseString <- case _pfType f of
+                          Td -> sh (TB.formatScientificBuilder TB.Fixed    (Just 0))    <$> numeric
+                          Tf -> sh (TB.formatScientificBuilder TB.Fixed    (_pfPrec f)) <$> numeric
+                          TF -> sh (TB.formatScientificBuilder TB.Fixed    (_pfPrec f)) <$> numeric
+                          Tg -> sh (TB.formatScientificBuilder TB.Generic  (_pfPrec f)) <$> numeric
+                          TG -> sh (TB.formatScientificBuilder TB.Generic  (_pfPrec f)) <$> numeric
+                          Te -> sh (TB.formatScientificBuilder TB.Exponent (_pfPrec f)) <$> numeric
+                          TE -> sh (TB.formatScientificBuilder TB.Exponent (_pfPrec f)) <$> numeric
+                          Tx -> sh (TB.hexadecimal . truncate @Scientific @Integer)     <$> numeric
+                          TX -> sh (TB.hexadecimal . truncate @Scientific @Integer)     <$> numeric
+                          Ts -> return $ case arg of
+                                             PString s -> TL.fromStrict s
+                                             _ -> TL.pack (show (pretty arg))
+                          _ -> throwPosError "sprintf: not yet supported"
+        (TB.fromLazyText baseString :) <$> go xs args
     go [] [] = return []
     go _ [] = throwPosError "sprintf: not enough arguments"
     go [] _ = [] <$ let msg = "sprintf: too many arguments" in checkStrict msg msg
