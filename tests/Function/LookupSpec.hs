a8bbaf89747171d06583cded5cedc18b398e3d8a
Pierre Radermecker <pierrer@pi3r.be>
2017-12-03 11:02:01 +0100

    Add specs to LookupSpec

--- tests/Function/LookupSpec.hs
+++ tests/Function/LookupSpec.hs
@@ -12,9 +12,11 @@ checkSuccess = checkExprsSuccess fname
 
 checkError :: [Expression] -> String -> Expectation
 checkError = checkExprsError fname
-
+ gs
 spec :: Spec
 spec = do
     it "should fail with no argument" (checkError [] "Expects one, two or three arguments")
     it "should succeed with one argument" (checkSuccess ["hostname"] "pure")
+    it "should succeed with two arguments, the second on being a datatype" (checkSuccess ["hostname", "String"] "pure")
+    it "should fail when the type mismatched" (pendingWith "Bug: lookup should fail on type mismatched" *> checkError ["hostname", "Array[String]"] "pure")
     it "should fail with two arguments both strings" (pendingWith "Bug: lookup should not accept a default as the second argument" *> checkError ["hostname", "default"] "pure")
