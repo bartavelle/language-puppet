69828967ddbe7a76b71897acb0ec1b77d68aa4e6
Pierre Radermecker <pierrer@pi3r.be>
2017-12-10 00:54:29 +0100

    Add pending spec for #229

--- tests/DT/Parser.hs
+++ tests/DT/Parser.hs
@@ -8,13 +8,14 @@ import           Text.Megaparsec (parse)
 
 spec :: Spec
 spec = do
-  let parsed s r = it s $ parse datatype "?" (toS s) `shouldParse` r
-      failed s = it s $ shouldFailOn (parse datatype "?") (toS s)
+  let parsed s r = it ("accepts " <> toS s) $ parse datatype "?" s `shouldParse` r
+      failed s = it ("rejects " <> toS s) $ shouldFailOn (parse datatype "?") s
   describe "String" $ do
     "String" `parsed` DTString Nothing Nothing
     failed "String[]"
     failed "String[4,5,6]"
     "String[5]" `parsed` DTString (Just 5) Nothing
     "String[5,8]" `parsed` DTString (Just 5) (Just 8)
+    it "accepts variables" $ pendingWith "to be fixed" *> parse datatype "?" "String[$var]" `shouldParse` DTString (Just 5) Nothing
   describe "Stdlib::" $ do
     "Stdlib::HTTPUrl" `parsed` DTData
