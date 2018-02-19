cb66d714e19bdb48a16e957455b455b33c1089dd
Pierre Radermecker <pierrer@pi3r.be>
2018-02-19 22:23:47 +0100

    Erb spec test: use local input instead of external files

--- tests/ErbSpec.hs
+++ tests/ErbSpec.hs
@@ -6,20 +6,18 @@ import           Test.Hspec
 
 import           Erb
 
-tests :: [(FilePath, [RubyStatement])]
+tests :: [(String, [RubyStatement])]
 tests =
-  [ ("simple.erb", [ Puts (Value (Literal "port = 5432\n" ))])
-  , ("var.erb", [ Puts (Value (Literal "mode = host=")), Puts (Object (Value (Literal "var"))), Puts (Value (Literal "\n"))])
+  [ ("port = 5432", [ Puts (Value (Literal "port = 5432" ))])
+  , ("mode = host=<% @var %>", [ Puts (Value (Literal "mode = host=")), Puts (Object (Value (Literal "var"))), Puts (Value (Literal ""))])
   ]
 
 alltests =
-  for_ tests $ \(fp, e) ->
-    test ("tests/erb/" <> fp) e
-  where
-    test fp expected = do
-      let item = it ("should parse " <> fp)
-      runIO (parseErbFile fp) >>= \case
-        Left err -> item $ expectationFailure (show err)
-        Right r -> item $ r `shouldBe` expected
+  for_ tests $ \(s, e) ->
+    let item = it ("should parse " <> s)
+    in
+    case parseErbString s of
+      Left err -> item $ expectationFailure (show err)
+      Right r -> item $ r `shouldBe` e
 
 spec = describe "Erb" $ alltests
