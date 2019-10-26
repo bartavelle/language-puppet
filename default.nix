c6f72688e0498e415a9915e6810dfe4f82a3c4e2
Pierre Radermecker <pierrer@pi3r.be>
2019-10-26 10:00:51 +0200

    Update nixpkgs pointer

--- default.nix
+++ default.nix
@@ -1,12 +1,13 @@
 # You can build this repository by running:
 #   $ nix-build
 {
-  pkgs ? import ./nix/nixpkgs.nix
+  nixpkgs ? (import ./nix/sources.nix).nixpkgs
 , compiler ? "default"
 }:
 
 
 let
+  pkgs = import nixpkgs {};
   filter = import ./nix/filter.nix;
   hpkgs = import ./nix/hpkgs.nix {inherit pkgs compiler;};
   haskellPackages = hpkgs.override {
